6cceea6d00e4abec271ab34b05bb88484b3283cc
Bruno251142 <bruno.f.souza@iesb.edu,br>
2026-03-27 20:23:08 -0300

    Finalização da parte 1 a 20.

--- praticas/projetoReact/vite-project/src/Components/Heading/index.tsx
+++ praticas/projetoReact/vite-project/src/Components/Heading/index.tsx
@@ -1,6 +1,7 @@
+import styles from "./style.module.css"
 type HeadingProps = {
 children: React.ReactNode; // Inicialmente, dizemos que children será apenas texto
 };
 export function Heading({children}: HeadingProps) {
-  return <h1>{children}</h1>
+  return <h1 className={styles.heading}>{children}</h1>
 } 
